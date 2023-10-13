0f4ceb0202214fcc188f528d899a385244b4cced
Richard Yu <richardyu042@gmail.com>
2023-10-13 05:17:17 -0400

    try remove axios dep

--- api/proxy.js
+++ api/proxy.js
@@ -1,21 +1,39 @@
+const http = require('http');
+const https = require('https');
 
-const axios = require('axios');
+const proxyServer = (req, res) => {
+    const { url, method, headers, data } = req.body;
 
-module.exports = async (req, res) => {
-  const { url, method, headers, data } = req.body;
+    const protocol = url.startsWith('https') ? https : http;
 
-  try {
-    const response = await axios({
-      method: method || 'GET',
-      url,
-      headers,
-      data
+    const requestOptions = {
+        method: method || 'GET',
+        headers: headers || {},
+    };
+
+    const proxyRequest = protocol.request(url, requestOptions, (proxyRes) => {
+        let responseData = '';
+
+        proxyRes.on('data', (chunk) => {
+            responseData += chunk;
+        });
+
+        proxyRes.on('end', () => {
+            res.writeHead(proxyRes.statusCode, proxyRes.headers);
+            res.end(responseData);
+        });
     });
 
-      console.log("proxy req: ", response);
-      res.status(200).json(response.data);
-  } catch (error) {
-      console.error("error: ", error)
-      res.status(500).json({ error: 'Internal Server Error' });
-  }
+    proxyRequest.on('error', (error) => {
+        console.error('Proxy Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    });
+
+    if (method === 'POST' || method === 'PUT' || method === 'PATCH') {
+        proxyRequest.write(JSON.stringify(data));
+    }
+
+    proxyRequest.end();
 };
+
+module.exports = proxyServer;
