4c8a174d27910868794510c14097d45fbe8f6011
Richard Yu <richardyu042@gmail.com>
2023-10-13 04:18:36 -0400

    init

---
+++ script.js
@@ -0,0 +1,32 @@
+// Define the Garmin KML URL
+const garminKMLUrl = 'https://share.garmin.com/Feed/Share/44RUF';
+
+// Create a Leaflet map centered at a specific location
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+// Add the OpenStreetMap tile layer to the map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Fetch the Garmin KML data and add it to the map
+fetch(garminKMLUrl)
+    .then(response => response.text())
+    .then(kmlText => {
+        const parser = new DOMParser();
+        const kmlDoc = parser.parseFromString(kmlText, 'text/xml');
+        
+        const trackCoordinates = kmlDoc.querySelectorAll('coordinates')[0].textContent.trim().split(' ');
+        const latlngs = trackCoordinates.map(coord => {
+            console.log(coord)
+            const [lng, lat] = coord.split(',').map(parseFloat);
+            return L.latLng(lat, lng);
+        });
+        
+        // Create a polyline from the coordinates and add it to the map
+        const polyline = L.polyline(latlngs, { color: 'red' }).addTo(map);
+        
+        // Fit the map to the polyline bounds
+        map.fitBounds(polyline.getBounds());
+    });
+
